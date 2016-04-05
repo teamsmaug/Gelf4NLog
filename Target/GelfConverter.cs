1d2f18bb8256e3da8c1964f302d2bbc8d532ce52
teamsmaug <web.smaug@tuinordic.com>
2016-04-05 10:07:47 +0200

    Changed flattened key seperator to underscore

--- Target/GelfConverter.cs
+++ Target/GelfConverter.cs
@@ -109,7 +109,7 @@ namespace Gelf4NLog.Target
                 {
                     var property = value as JProperty;
 
-                    var flattenedKey = string.Concat(key, ".", property.Name);
+                    var flattenedKey = string.Concat(key, "_", property.Name);
                     if (!property.Value.HasValues)
                     {
                         gelfMessage.Add(flattenedKey, property.Value);
